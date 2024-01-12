6792cf26dd93aa4a8dbcdd5df4e732b62b2738c2
Dav-Sandhu <davanjit@hotmail.com>
2024-01-12 14:06:52 -0500

    fixed spacing issue on the backend

--- server/index.js
+++ server/index.js
@@ -9,93 +9,93 @@ config()
 app.use(cors())
 
 const queries = (type, values) => {
-    switch(type){
-      case "find-employee":
-        return {
-          query: `USE WorkForce; SELECT * FROM employee WHERE employee_number=@employee_number AND password=@password`,
-          parameters: [
-            { name: 'employee_number', type: TYPES.Int, value: parseInt(values[0] !== NaN ? parseInt(values[0]) : -1) },
-            { name: 'password', type: TYPES.VarChar, value: values[1] }
-          ]
-        }
-      case "picture":
-        return {
-          query: `USE WorkForce; SELECT picture FROM employee;`,
-          parameters: []
-        }
-      case "daily-report":
-        return{
-          query: `USE WorkForce; SELECT * FROM daily_report`,
-          parameters: []
-        }
-      case "email":
-        return{
-          query: `USE WorkForce; SELECT * FROM employee WHERE email=@email`,
-          parameters: [{name: 'email', type: TYPES.VarChar, value: values[0]}]
-        }
-      default:
-        return;
-    }
+  switch(type){
+    case "find-employee":
+      return {
+        query: `USE WorkForce; SELECT * FROM employee WHERE employee_number=@employee_number AND password=@password`,
+        parameters: [
+          { name: 'employee_number', type: TYPES.Int, value: parseInt(values[0] !== NaN ? parseInt(values[0]) : -1) },
+          { name: 'password', type: TYPES.VarChar, value: values[1] }
+        ]
+      }
+    case "picture":
+      return {
+        query: `USE WorkForce; SELECT picture FROM employee;`,
+        parameters: []
+      }
+    case "daily-report":
+      return{
+        query: `USE WorkForce; SELECT * FROM daily_report`,
+        parameters: []
+      }
+    case "email":
+      return{
+        query: `USE WorkForce; SELECT * FROM employee WHERE email=@email`,
+        parameters: [{name: 'email', type: TYPES.VarChar, value: values[0]}]
+      }
+    default:
+      return;
+  }
   }
 
 app.get('/sql', (req, res) => {
     
-    const input = JSON.parse(req.query.query)
-    const query = queries(input.type, input.values)
+  const input = JSON.parse(req.query.query)
+  const query = queries(input.type, input.values)
 
-    let output = []
+  let output = []
 
-    const connection = new Connection({
-        server: process.env.domain,
-        authentication: {
-            type: 'default',
-            options: {
-                userName: process.env.USER,
-                password: process.env.PASSWORD 
-            }
-        },
-        options: {
-            database: process.env.DATABASE,
-            port: parseInt(process.env.PORT),
-            trustServerCertificate: true,
-            encrypt: true
-        }
+  const connection = new Connection({
+    server: process.env.domain,
+    authentication: {
+      type: 'default',
+      options: {
+        userName: process.env.USER,
+        password: process.env.PASSWORD 
+      }
+    },
+    options: {
+      database: process.env.DATABASE,
+      port: parseInt(process.env.PORT),
+      trustServerCertificate: true,
+      encrypt: true
+    }
+  })
+  
+  connection.connect(err => {
+    if(!err){
+      const request = new Request(query.query, err => {
+        err ? console.log(err) : ""
+      })
+
+      //adds the parameters to the request separately to prevent sql injection
+      query.parameters.forEach(p => {
+        request.addParameter(p.name, p.type, p.value)
+      })
+
+    request.on('row', function(columns){ 
+        let result = {}
+
+        columns.forEach(function(column){
+          result[column.metadata.colName] = column.value
+        }) 
+
+        output.push(result)
+    })  
+          
+    request.on("requestCompleted", function(){
+        res.send(output)
+        connection.close()
     })
-    
-    connection.connect(err => {
-        if(!err){
-            const request = new Request(query.query, err => {
-                err ? console.log(err) : ""
-            })
 
-            //adds the parameters to the request separately to prevent sql injection
-            query.parameters.forEach(p => {
-                request.addParameter(p.name, p.type, p.value)
-            })
-
-            request.on('row', function(columns){ 
-                let result = {}
-    
-                columns.forEach(function(column){
-                    result[column.metadata.colName] = column.value
-                }) 
-    
-                output.push(result)
-            })  
-                
-            request.on("requestCompleted", function(){
-				        res.send(output)
-                connection.close()
-            })
-    
-            request.on("error", function(err){
-              res.send(output)
-              console.log(err)
-            })
-            
-            connection.execSql(request) 
-        }
+    request.on("error", function(err){
+      res.send(output)
+      console.log(err)
     })
+      
+    connection.execSql(request) 
+  }
+  })
 })
 
 app.listen(3000)
