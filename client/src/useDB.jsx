0d11c2961ed232388c6a8a0c4716b3f7938756e9
Dav-Sandhu <davanjit@hotmail.com>
2024-01-14 16:56:13 -0500

    got rid of the need for DBPORT

--- client/src/useDB.jsx
+++ client/src/useDB.jsx
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const makeRequest = async (req) => {
 
-    const url = 'https://workforce-app.azurewebsites.net/sql'
+    const url = import.meta.env.VITE_SQLDB || 'https://workforce-app.azurewebsites.net/sql'
 
     const res = await axios.get(url, {
         params: {
