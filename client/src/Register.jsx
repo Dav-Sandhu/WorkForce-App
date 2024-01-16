5c74ff7398697a9bea574ae359575bc03a9efc20
Dav-Sandhu <davanjit@hotmail.com>
2024-01-16 18:13:32 -0500

    minor changes to registration form

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -52,58 +52,60 @@ const Register = () => {
 
                         <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
 
-                        <form className="mx-1 mx-md-4">
+                        <form className="mx-1 mx-md-4" onSubmit={(e) => {
+                            e.preventDefault()
+                        }}>
 
-                        <div className="d-flex flex-row align-items-center mb-4">
-                            <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                            <div className="form-outline flex-fill mb-0">
-                            <input type="text" id="first_name" className="form-control" />
-                            <label className="form-label" htmlFor="first_name">First Name</label>
+                            <div className="d-flex flex-row align-items-center mb-4">
+                                <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                                <div className="form-outline flex-fill mb-0">
+                                <input type="text" id="first_name" className="form-control" />
+                                <label className="form-label" htmlFor="first_name">First Name</label>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="d-flex flex-row align-items-center mb-4">
-                            <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                            <div className="form-outline flex-fill mb-0">
-                            <input type="text" id="last_name" className="form-control" />
-                            <label className="form-label" htmlFor="last_name">Last Name</label>
+                            <div className="d-flex flex-row align-items-center mb-4">
+                                <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                                <div className="form-outline flex-fill mb-0">
+                                <input type="text" id="last_name" className="form-control" />
+                                <label className="form-label" htmlFor="last_name">Last Name</label>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="d-flex flex-row align-items-center mb-4">
-                            <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                            <div className="form-outline flex-fill mb-0">
-                            <input type="email" id="email" className="form-control" />
-                            <label className="form-label" htmlFor="email">Your Email</label>
+                            <div className="d-flex flex-row align-items-center mb-4">
+                                <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                <div className="form-outline flex-fill mb-0">
+                                <input type="email" id="email" className="form-control" />
+                                <label className="form-label" htmlFor="email">Your Email</label>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="d-flex flex-row align-items-center mb-4">
-                            <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                            <div className="form-outline flex-fill mb-0">
-                            <input type="password" id="password" className="form-control" />
-                            <label className="form-label" htmlFor="password">Password</label>
+                            <div className="d-flex flex-row align-items-center mb-4">
+                                <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                <div className="form-outline flex-fill mb-0">
+                                <input type="password" id="password" className="form-control" />
+                                <label className="form-label" htmlFor="password">Password</label>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="d-flex flex-row align-items-center mb-4">
-                            <i className="fas fa-key fa-lg me-3 fa-fw"></i>
-                            <div className="form-outline flex-fill mb-0">
-                            <input type="password" id="repeat_password" className="form-control" />
-                            <label className="form-label" htmlFor="repeat_password">Repeat your password</label>
+                            <div className="d-flex flex-row align-items-center mb-4">
+                                <i className="fas fa-key fa-lg me-3 fa-fw"></i>
+                                <div className="form-outline flex-fill mb-0">
+                                <input type="password" id="repeat_password" className="form-control" />
+                                <label className="form-label" htmlFor="repeat_password">Repeat your password</label>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="form-check d-flex justify-content-center mb-5">
-                            <input className="form-check-input me-2" type="checkbox" value="" id="terms" />
-                            <label className="form-check-label" htmlFor="terms">
-                            I agree all statements in <a href="#!">Terms of service</a>
-                            </label>
-                        </div>
+                            <div className="form-check d-flex justify-content-center mb-5">
+                                <input className="form-check-input me-2" type="checkbox" value="" id="terms" />
+                                <label className="form-check-label" htmlFor="terms">
+                                I agree all statements in <a href="#!">Terms of service</a>
+                                </label>
+                            </div>
 
-                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                            <button type="button" className="btn btn-primary btn-lg">Register</button>
-                        </div>
+                            <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                                <input type="submit" className="btn btn-primary btn-lg" value="Register" />
+                            </div>
 
                         </form>
 
