24b4cad7dc9e30553f9a446cc5afef18c19076fb
Dav-Sandhu <davanjit@hotmail.com>
2024-02-21 10:19:44 -0500

    fixed height issue in admin section

--- client/src/Admin/Navbar.jsx
+++ client/src/Admin/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = ({ children }) => {
                 <div class="container-fluid"><span className="navbar-brand">SFM WORKFORCE</span></div>
             </nav>
             <div className="container-fluid">
-                <div className="row flex-nowrap">
+                <div className="row flex-nowrap grid-container">
                     <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-purple">
                         <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                             <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
