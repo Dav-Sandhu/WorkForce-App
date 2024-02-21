a89a9a159bb72260c39333f240552babbb628120
Dav-Sandhu <davanjit@hotmail.com>
2024-02-21 11:53:24 -0500

    added way to return to home from admin screen under supervisor user

---
+++ client/dist/assets/Spinner.a8c3e92e.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index.c1e64761.js";const r=()=>s("div",{className:"spinner-border",role:"status",children:s("span",{className:"visually-hidden",children:"Loading..."})});export{r as default};
--- client/src/Admin/Navbar.jsx
+++ client/src/Admin/Navbar.jsx
@@ -1,6 +1,12 @@
 import "./Navbar.scss"
+import { useUserInfo } from "../UserProvider" 
+import { useNavigate } from "react-router-dom"
 
 const Navbar = ({ children }) => {
+   
+    const navigate = useNavigate()
+    const user = useUserInfo()
+   
     return(
         <>
             <nav class="navbar navbar-dark bg-dark">
@@ -11,6 +17,14 @@ const Navbar = ({ children }) => {
                     <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-purple">
                         <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                             <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                                {
+                                    user.userInfo.is_supervisor ? 
+                                    <li className="nav-item navHeading">
+                                        <a className="nav-link align-middle px-0" href="/">
+                                            <span className="ms-1 d-none d-sm-inline navBtn">Home</span>
+                                        </a>
+                                    </li> : ""
+                                }
                                 <li className="nav-item active navHeading">
                                     <a className="nav-link align-middle px-0" href="/admin">
                                         <span className="ms-1 d-none d-sm-inline navBtn">Reports</span>
